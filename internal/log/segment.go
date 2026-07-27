5ebc984729f7341d3b938fab7755c39dd4f1a827
hrtsegv <abdullahalaadine63@gmail.com>
2026-07-27 11:04:21 +0300

    add Close method to segment struct for releasing resources

--- internal/log/segment.go
+++ internal/log/segment.go
@@ -104,3 +104,15 @@ func (s *segment) IsMaxed() bool {
 	return s.store.size >= s.config.Segment.MaxStoreBytes ||
 		s.index.size >= s.config.Segment.MaxIndexBytes
 }
+
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
