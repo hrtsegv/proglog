c1e17bf791d9364c30dad8decaba52f212750708
hrtsegv <abdullahalaadine63@gmail.com>
2026-07-28 04:31:08 +0300

    add HighestOffset method to Log for retrieving the highest segment offset

--- internal/log/log.go
+++ internal/log/log.go
@@ -147,3 +147,15 @@ func (l *Log) LowestOffset() (uint64, error) {
 
 	return l.segments[0].baseOffset, nil
 }
+
+func (l *Log) HighestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	off := l.segments[len(l.segments)-1].nextOffset
+	if off == 0 {
+		return 0, nil
+	}
+
+	return off - 1, nil
+}
