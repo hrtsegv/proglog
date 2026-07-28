8e9796cf7c3a575891273f37638cf39710d231c2
hrtsegv <abdullahalaadine63@gmail.com>
2026-07-28 03:56:09 +0300

    add NewLog function for initializing Log with default segment sizes

--- internal/log/log.go
+++ internal/log/log.go
@@ -9,3 +9,17 @@ type Log struct {
 	activeSegment *segment
 	segments      []*segment
 }
+
+func NewLog(dir string, c Config) (*Log, error) {
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+	l := &Log{
+		Dir:    dir,
+		Config: c,
+	}
+	return l, l.setup()
+}
